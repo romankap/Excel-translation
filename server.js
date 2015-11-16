93cdb1f42ff50c7f9a31e1a9cc6c1ee072976e80
romankap <romankap@gmail.com>
2015-11-16 11:27:02 +0200

    Second update of missing files

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var http = require('http');
+var fs = require('fs');
+var config = require('./config');
+var multer  = require('multer')
+var upload  = multer({ dest: __dirname + './sheets/'});
+
+
+var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true})); // support encoded bodies
+
+
+// Serve HTTP requests
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+
+// =====================
+/*app.use(multer({ dest: __dirname + '/sheets/',
+    rename: function (fieldname, filename) {
+        return filename+Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+    }
+}));*/
+
+app.post('/api/upload-sheet', upload.single('sheet'), function (req, res) {
+    console.log('Uploaded to  ' + req.file.path + "\n" + JSON.stringify(req.file));
+    
+    //var download_buffer = new Buffer(fs.readFileSync())
+    res.sendFile(__dirname + "/download.html");
+})
+
+
+app.get('/', function(request, response) {
+    //fs.readFileSync("index.html");
+    var index_buffer = new Buffer(fs.readFileSync("index.html"))
+    response.send(index_buffer.toString())
+});
+
+// ====== Default case ========
+
+app.get('*', function(request, response) {
+    //fs.readFileSync("index.html");
+    var index_buffer = new Buffer(fs.readFileSync("index.html"))
+    response.send(index_buffer.toString())
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
