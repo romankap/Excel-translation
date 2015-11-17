db5c8e94c2b2a4f7850340c009aecfd70d489e35
romankap <romankap@gmail.com>
2015-11-17 15:19:10 +0200

    Upload and read file in index.js

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ var test = function() {
     console.log("hello");
 }
 test();
+
+
+function handleFile(e) {
+  var files = e.target.files;
+  var i,f;
+  for (i = 0, f = files[i]; i != files.length; ++i) {
+    var reader = new FileReader();
+    var name = f.name;
+    reader.onload = function(e) {
+      var data = e.target.result;
+
+      var workbook = XLSX.read(data, {type: 'binary'});
+
+      console.log("Inside handleFile(1)");
+        
+    };
+      reader.readAsBinaryString(f);
+      console.log("Inside handleFile(2)");
+      $("#download-btn").show();
+      
+  }
+}
+
+
+window.onload = function() {
+    var fileInput = document.getElementById('fileInput');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+    fileInput.addEventListener('change', handleFile, false);
+    
+    $("#download-btn").hide();
+}
