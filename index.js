ba7b20a691fd69c2cb09f5d1e3d636f31857b984
romankap <romankap@gmail.com>
2015-11-19 00:58:46 +0200

    Demo of replacing first string in dictionar

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ var test = function() {
 }
 test();
 
+var dictionary = {};
+
+function handleTranslationFile(evt) {
+  var files = evt.target.files; 
+    		
+    if (files) {
+        for (var i=0, f; f=files[i]; i++) {
+	          var r = new FileReader();
+            r.onload = (function(f) {
+                return function(e) {
+                    var contents = e.target.result;
+                    contents.split("\n").forEach(function(line, index, arr) {
+                        if (index === arr.length - 1 && line === "") { return; }
+                        var russ = line.substr(0,line.indexOf(','));
+                        var heb = line.substr(line.indexOf(',')+1);
+
+                        dictionary[russ] = heb;
+                        
+                        console.log(index + ". Russian: " + russ + ". Hebrew: " + dictionary[russ]);
+                      });
+                };
+            })(f);
+
+            r.readAsText(f);
+        }   
+    } else {
+	      alert("Failed to load files"); 
+    }
+}
+
 
 function handleFile(e) {
   var files = e.target.files;
@@ -11,16 +41,43 @@ function handleFile(e) {
     var reader = new FileReader();
     var name = f.name;
     reader.onload = function(e) {
-      var data = e.target.result;
+        var data = e.target.result;
 
-      var workbook = XLSX.read(data, {type: 'binary'});
+        var workbook = XLSX.read(data, {type: 'binary'});
+        var outputWorkbook = XLSX.read(data, {type: 'binary'});
 
-      console.log("Inside handleFile(1)");
+        var sheet_name_list = workbook.SheetNames;
+        
+        sheet_name_list.forEach(function(y) { /* iterate through sheets */
+            var worksheet = workbook.Sheets[y];
+            var output_sheet = outputWorkbook.Sheets[y];
+            
+            for (z in worksheet) {
+                /* all keys that do not begin with "!" correspond to cell addresses */
+                if(z[0] === '!') continue;
+                console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v).replace("Заказчик", dictionary["Заказчик"])  );
+                output_sheet[z].v = JSON.stringify(worksheet[z].v).replace("Заказчик", dictionary["Заказчик"])
+            }
+        });
+        
+        var wopts = { bookType:'xlsx', bookSST:false, type:'binary' };
+        var wbout = XLSX.write(outputWorkbook,wopts);
+
+        function s2ab(s) {
+          var buf = new ArrayBuffer(s.length);
+          var view = new Uint8Array(buf);
+          for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+          return buf;
+        }
+
+        /* the saveAs call downloads a file on the local machine */
+        saveAs(new Blob([s2ab(wbout)],{type:""}), "test.xlsx")
         
     };
-      reader.readAsBinaryString(f);
-      console.log("Inside handleFile(2)");
-      $("#download-btn").show();
+    // End of onload
+    reader.readAsBinaryString(f);
+    console.log("Inside handleFile(2)");
+    $("#download-btn").show();
       
   }
 }
@@ -28,9 +85,11 @@ function handleFile(e) {
 
 window.onload = function() {
     var fileInput = document.getElementById('fileInput');
+    var translationFileInput = document.getElementById('translationFileInput');
     var fileDisplayArea = document.getElementById('fileDisplayArea');
 
     fileInput.addEventListener('change', handleFile, false);
+    translationFileInput.addEventListener('change', handleTranslationFile, false);
     
     $("#download-btn").hide();
 }
